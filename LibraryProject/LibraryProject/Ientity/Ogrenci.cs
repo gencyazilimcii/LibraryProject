5b6c1b8aa8f5eca89981c08df7e1153d80862f18
gencyazilimcii <serkaniisler@gmail.com>
2023-02-21 16:59:18 +0300

    LibraryProject
    C# la Katmanlı Mimari Kullanarak Kütüphane Otomasyonu

---
+++ LibraryProject/LibraryProject/Ientity/Ogrenci.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryProject.Ientity
+{
+    class Ogrenci
+    {
+        public int ogrNo;
+        public string ogrAd;
+
+        public int ogrNoSet
+        {
+            get { return ogrNoSet; }
+            set { ogrNoSet = value; }
+        }
+            
+    }
+
+    
+
+}
---
+++ LibraryProject/LibraryProject/Ientity/Yazar.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryProject.Ientity
+{
+    class Yazar
+    {
+        public int yazarNo;
+        public string yazarAd;
+        public string yazarOzgecmis;
+
+
+        public int yazarNoSet
+        {
+            get { return yazarNo; }
+            set { yazarNo = value; }
+        }
+
+        public string yazarAdSet
+        {
+            get { return yazarAd; }
+            set { yazarAd = value; }
+        }
+
+        public string yazarOzgecmisSet
+        {
+            get { return yazarOzgecmis; }
+            set { yazarOzgecmis = value; }
+        }
+
+        public Yazar()
+        {
+            yazarNo = 0;
+            yazarAd = "";
+            yazarOzgecmis = "";
+
+        }
+
+    }
+}
